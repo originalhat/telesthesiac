60b3bc8ad5d0dd17ae62eb328316c6c6d3f6e3f0
Devin Brown <devinb@ucar.edu>
2013-05-04 17:59:08 -0600

    Adding rails files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Neko::Application.config.secret_token = 'ccc09806c1eb5f5a06fd1d93e52915a1cc944b9822900e3082e829d219072fdea81e8ca96e47cc0f847151ec1b6bd5e99dbcc5bddf15eda95788e0463857db24'
